e7cd9aebd17c4bf1cf01441a5403e56a71a4dee9
Ted Guan <tedguan2005@gmail.com>
2022-09-19 08:46:24 -0700

    exercises 1.1,1.2 working

--- p1-course-info/src/App.js
+++ p1-course-info/src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>Welcome to {props.course}</h1>
+    </div>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.part}</p>
+      <p>{props.exercise}</p>
+    </div>
+  );
+};
+
+const Total = (props) => {};
 
 function App() {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header course={course} />
+      <Part part={part1} exercise={exercises1} />
+      <Part part={part2} exercise={exercises2} />
+      <Part part={part3} exercise={exercises3} />
+    </>
   );
 }
 
